bc20fcbe1712e18ba52ae3504366b7e41231ef94
Anisenko Elvira <elvira.anisenko@yandex.ru>
2023-10-25 12:16:22 +0300

    Initial commit

---
+++ src/main/java/GameComparator.java
@@ -0,0 +1,11 @@
+import java.util.Comparator;
+
+public class GameComparator implements Comparator <Player> {
+    @Override
+    public int compare(Player o1, Player o2) {
+        if() {};
+        else {}:
+        else {
+        return 0};
+    }
+}
